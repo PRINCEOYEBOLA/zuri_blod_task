86455eb59fdfab06fc4e719bf3dc609a42cad854
Iamoye <odeleyebola@gmail.com>
2022-06-14 07:57:55 +0100

    I corrected the published date and makemigrations as wellas migrate.

--- I4G014715HDK/blog/models.py
+++ I4G014715HDK/blog/models.py
@@ -10,7 +10,6 @@ class Post(models.Model):
     Title = models.CharField(max_length=200)
     Text = models.TextField()
     Author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete =models.CASCADE)
-    Created_date = models.DateTimeField(auto_now_add=True)
-    Published_date = models.DateTimeField
-    
+    Created_date = models.DateTimeField(auto_now_add = True)
+    Published_date = models.DateTimeField()
     
